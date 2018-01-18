7260e46b3cb3ca021f74187df58b36005729f3f6
Mikko Kotola <mikko.kotola@helsinki.fi>
2018-01-18 12:09:52 +0200

    Enviroment path.

--- code/t60-_blogibackend/index.js
+++ code/t60-_blogibackend/index.js
@@ -10,7 +10,7 @@ const Blog = require('./models/blog')
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.logger)
-//app.use(middleware.error)
+app.use(middleware.error)
 
 if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
@@ -38,7 +38,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
